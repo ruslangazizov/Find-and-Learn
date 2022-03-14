f0cb748f69a7166040aeccc6ea5acc35e60b9ffe
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2022-03-14 14:15:30 +0300

    added lock for portrait orientation

--- Supporting Files/AppDelegate.swift
+++ Supporting Files/AppDelegate.swift
@@ -10,11 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var orientationLock: UIInterfaceOrientationMask = .portrait
     
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        return orientationLock
     }
 }
