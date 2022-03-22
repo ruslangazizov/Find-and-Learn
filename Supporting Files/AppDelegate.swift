0b37875c6047e1621ad69c05eff1f2beea363a9d
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2022-03-22 08:47:29 +0300

    removed controller from app delegate

--- Supporting Files/AppDelegate.swift
+++ Supporting Files/AppDelegate.swift
@@ -19,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = RegistrationViewController()
         return true
     }
     
