d3d65595c994105e05ee1b14ddb19d779627974c
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2022-03-23 13:10:55 +0300

    new injection for ouput into VC

--- Source/Scenarios/Entrance/Modules/Password Recovery/PasswordRecoveryViewController.swift
+++ Source/Scenarios/Entrance/Modules/Password Recovery/PasswordRecoveryViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 final class PasswordRecoveryViewController: UIViewController {
     // MARK: Dependencies
     
-    var output: PasswordRecoveryViewOutput?
-
+    private var output: PasswordRecoveryViewOutput
+    
+    // MARK: Init
+    
+    init(output: PasswordRecoveryViewOutput) {
+        self.output = output
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
     }
 }
 
