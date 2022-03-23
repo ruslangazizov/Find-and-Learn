ae409e335dff9124cb562f7f704f73e6cddcb483
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2022-03-23 12:53:57 +0300

    created presenter

---
+++ Source/Scenarios/Entrance/Modules/Password Recovery/PasswordRecoveryPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  PasswordRecoveryPresenter.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 23.03.2022.
+//
+
+import Foundation
+
+final class PasswordRecoveryPresenter {
+    
+}
