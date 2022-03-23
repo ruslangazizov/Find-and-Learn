8a79c8a6baf48309fe06c4ccd7f9a1d5f576527f
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2022-03-23 12:52:31 +0300

    created vc

---
+++ Source/Scenarios/Entrance/Modules/Password Recovery/PasswordRecoveryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PasswordRecoveryViewController.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 23.03.2022.
+//
+
+import UIKit
+
+final class PasswordRecoveryViewController: UIViewController {
+
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
