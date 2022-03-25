8313794a13b92f897f8693f4e0ff0ddc9f72649a
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2022-03-25 10:22:22 +0300

    created auth vc

---
+++ Source/Scenarios/Entrance/Modules/Authorization/AuthorizationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthorizationViewController.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 25.03.2022.
+//
+
+import UIKit
+
+final class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
