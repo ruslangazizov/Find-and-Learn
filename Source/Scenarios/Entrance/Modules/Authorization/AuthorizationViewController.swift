65c0e24705d719477129e4a55463916716a48a20
Snitsaryuk Roman <71031730+Sixezr@users.noreply.github.com>
2022-03-25 10:31:18 +0300

    implemented base for vc

--- Source/Scenarios/Entrance/Modules/Authorization/AuthorizationViewController.swift
+++ Source/Scenarios/Entrance/Modules/Authorization/AuthorizationViewController.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 final class AuthorizationViewController: UIViewController {
+    // MARK: Ui
+    
+    // MARK: Dependencies
+    
+    private var presenter: AuthorizationViewOutput
+    
+    // MARK: Init
+    
+    init(presenter: AuthorizationViewOutput) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+// MARK: - ViewInput
+
+extension AuthorizationViewController: AuthorizationViewInput {
 }
