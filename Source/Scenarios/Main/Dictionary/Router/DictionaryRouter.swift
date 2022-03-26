c336a365260ef515fad83134f4b2846d2a64192b
Ruslan <ruslangazizov36@gmail.com>
2022-03-27 00:59:04 +0300

    Add DictionaryRouter

---
+++ Source/Scenarios/Main/Dictionary/Router/DictionaryRouter.swift
@@ -0,0 +1,31 @@
+//
+//  DictionaryRouter.swift
+//  Find-and-Learn
+//
+//  Created by Руслан on 25.03.2022.
+//
+
+import Foundation
+import UIKit
+
+class DictionaryRouter: DictionaryRouterInput {
+    weak var view: UINavigationController?
+    
+    func showFavoriteWords() {
+        let mockFavoriteWordsViewController = UIViewController()
+        mockFavoriteWordsViewController.view.backgroundColor = .green
+        view?.pushViewController(mockFavoriteWordsViewController, animated: true)
+    }
+    
+    func showHistoryWords() {
+        let mockHistoryWordsViewController = UIViewController()
+        mockHistoryWordsViewController.view.backgroundColor = .systemPink
+        view?.pushViewController(mockHistoryWordsViewController, animated: true)
+    }
+    
+    func showWordDetail(_ word: Word) {
+        let mockWordDetailViewController = UIViewController()
+        mockWordDetailViewController.view.backgroundColor = .cyan
+        view?.pushViewController(mockWordDetailViewController, animated: true)
+    }
+}
