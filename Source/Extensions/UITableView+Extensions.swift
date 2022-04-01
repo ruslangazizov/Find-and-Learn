7bdc28a538ddd0ce6dc3dc96374209ada85476a3
Ruslan Gazizov <ruslangazizov36@gmail.com>
2022-04-01 17:42:43 +0300

    Add extensions for UITableView and move AppDelegate to Source directory

---
+++ Source/Extensions/UITableView+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UITableView+register+dequeue.swift
+//  Find-and-Learn
+//
+//  Created by Руслан on 01.04.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UITableView {
+    func register(_ cellTypes: UITableViewCell.Type...) {
+        cellTypes.forEach { cellType in
+            register(cellType.self, forCellReuseIdentifier: "\(cellType.self)")
+        }
+    }
+    
+    func dequeue<T: UITableViewCell>(_ cellType: T.Type, for indexPath: IndexPath) -> T {
+        // swiftlint:disable:next force_cast
+        return dequeueReusableCell(withIdentifier: "\(cellType.self)", for: indexPath) as! T
+    }
+}
