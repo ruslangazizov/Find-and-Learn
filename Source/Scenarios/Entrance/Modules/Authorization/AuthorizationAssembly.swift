464ddc7ca85125fa9bde818dcba2938b58a20837
Snitsaryuk Roman <nits14@mail.ru>
2022-04-01 23:10:47 +0300

    add assembly layer

---
+++ Source/Scenarios/Entrance/Modules/Authorization/AuthorizationAssembly.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorizationAssembly.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 01.04.2022.
+//
+
+import Foundation
+import UIKit
+
+final class AuthorizationAssembly: AssemblyProtocol {
+    func assemble() -> UIViewController {
+        let presenter = AuthorizationPresenter()
+        
+        let viewController = AuthorizationViewController(presenter: presenter)
+        presenter.view = viewController
+        
+        return viewController
+    }
+}
