dbff9b24c52f46b4f8d304240b3fff823ebdc61a
Snitsaryuk Roman <nits14@mail.ru>
2022-04-02 13:37:54 +0300

    added assembly

---
+++ Source/Scenarios/Main/Account/AccountAssembly.swift
@@ -0,0 +1,20 @@
+//
+//  AccountAssembly.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 02.04.2022.
+//
+
+import Foundation
+import UIKit
+
+final class AccountAssembly: AssemblyProtocol {
+    func assemble() -> UIViewController {
+        let presenter = AccountPresenter()
+        
+        let viewController = AccountViewController(presenter: presenter)
+        presenter.view = viewController
+        
+        return viewController
+    }
+}
