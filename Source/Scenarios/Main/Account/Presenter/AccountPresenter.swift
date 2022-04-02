2e9623ce932a44c75ff35a87a6dd323a48f31f8c
Snitsaryuk Roman <nits14@mail.ru>
2022-04-02 12:53:00 +0300

    added presenter

---
+++ Source/Scenarios/Main/Account/Presenter/AccountPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  AccountPresenter.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 02.04.2022.
+//
+
+import Foundation
+
+final class AccountPresenter: AccountViewOutput {
+    // MARK: - Dependencies
+    
+    weak var view: AccountViewInput?
+}
