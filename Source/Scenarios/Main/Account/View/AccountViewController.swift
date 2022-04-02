4d069887cd6f8abf36f726e450e211b852814767
Snitsaryuk Roman <nits14@mail.ru>
2022-04-02 13:06:01 +0300

    added base for vc

--- Source/Scenarios/Main/Account/View/AccountViewController.swift
+++ Source/Scenarios/Main/Account/View/AccountViewController.swift
@@ -8,8 +8,41 @@
 import UIKit
 
 final class AccountViewController: UIViewController {
-
+    // MARK: UI
+    
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    // MARK: Dependencies
+    
+    private let presenter: AccountViewOutput
+    
+    // MARK: Init
+    
+    init(presenter: AccountViewOutput) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: Private
+    
+    private func setupLayout() {
+    }
+}
+
+// MARK: - ViewInput
+
+extension AccountViewController: AccountViewInput {
 }
