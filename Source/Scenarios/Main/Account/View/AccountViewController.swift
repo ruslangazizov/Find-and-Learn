c0fe4162967f799f592b03638a90611e45942772
Snitsaryuk Roman <nits14@mail.ru>
2022-04-02 12:48:52 +0300

    created vc

---
+++ Source/Scenarios/Main/Account/View/AccountViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AccountViewController.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 02.04.2022.
+//
+
+import UIKit
+
+final class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
