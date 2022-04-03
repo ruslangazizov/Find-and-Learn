e2052487296245e868e4bc8549c3b275defcf07b
Snitsaryuk Roman <nits14@mail.ru>
2022-04-03 13:27:43 +0300

    added table view

--- Source/Scenarios/Main/Account/View/AccountViewController.swift
+++ Source/Scenarios/Main/Account/View/AccountViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 final class AccountViewController: UIViewController {
     // MARK: UI
     
+    private lazy var headerView: UIView = {
+        return UIView()
+    }()
+    
     private lazy var avatarView: CircularView = {
         return CircularView()
     }()
@@ -19,18 +23,21 @@ final class AccountViewController: UIViewController {
             placeholder: R.string.localizable.account_screen_user_name_placeholder(),
             layerColor: nil
         )
+        textField.textAlignment = .center
         return textField
     }()
     
     private lazy var tableView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.keyboardDismissMode = .onDrag
+        tableView.backgroundColor = .systemBackground
         return tableView
     }()
     
     // MARK: Dependencies
     
     private let presenter: AccountViewOutput
+    private let settings: [Settings] = []
     
     // MARK: Init
     
@@ -60,6 +67,20 @@ final class AccountViewController: UIViewController {
     }
     
     private func setupLayout() {
+        headerView.addSubview(avatarView)
+        avatarView.snp.makeConstraints { make in
+            make.height.equalTo(avatarView.snp.width)
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(view.frame.width / .multiplier)
+        }
+
+        headerView.addSubview(userNameTextField)
+        userNameTextField.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(view.frame.width / .multiplier)
+            make.top.equalTo(avatarView.snp.bottom).offset(Constants.bottomOffset)
+            make.bottom.equalToSuperview().inset(Constants.bottomInset)
+        }
+
         view.addSubview(tableView)
         tableView.snp.makeConstraints { make in
             make.leading.bottom.trailing.top.equalToSuperview()
@@ -76,12 +97,31 @@ extension AccountViewController: AccountViewInput {
 
 extension AccountViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return settings.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeue(AccountTableViewCell.self, for: indexPath)
-        
+        cell.configure(with: settings[indexPath.row])
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return headerView
+    }
+}
+
+// MARK: - Constants
+
+private extension AccountViewController {
+    enum Constants {
+        static let topInset = 50
+        static let bottomInset = 15
+        
+        static let bottomOffset = 20
+    }
+}
+
+private extension CGFloat {
+    static let multiplier: CGFloat = 4
 }
