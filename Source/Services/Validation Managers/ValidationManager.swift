3910aa9e0d47f0e515e6807a476bc871e512d808
Snitsaryuk Roman <nits14@mail.ru>
2022-04-03 19:17:50 +0300

    create implementation of validation manager

---
+++ Source/Services/Validation Managers/ValidationManager.swift
@@ -0,0 +1,12 @@
+//
+//  ValidationManager.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 03.04.2022.
+//
+
+import Foundation
+
+final class ValidationManager: ValidationManagerProtocol {
+    
+}
