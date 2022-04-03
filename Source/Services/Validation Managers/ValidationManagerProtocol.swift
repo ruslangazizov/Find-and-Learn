92a4e4aeb3f0a99b971dc9351cc1f7713c173269
Snitsaryuk Roman <nits14@mail.ru>
2022-04-03 19:16:26 +0300

    created validation protocol

---
+++ Source/Services/Validation Managers/ValidationManagerProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  ValidationManagerProtocol.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 03.04.2022.
+//
+
+import Foundation
+
+protocol ValidationManagerProtocol: AnyObject {
+}
