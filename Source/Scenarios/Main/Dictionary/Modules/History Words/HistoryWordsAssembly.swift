0c924d0faf95de3597d9c064ceef394fdb81b4f3
Ruslan Gazizov <ruslangazizov36@gmail.com>
2022-04-05 13:26:12 +0300

    Add assembly and router implementations

---
+++ Source/Scenarios/Main/Dictionary/Modules/History Words/HistoryWordsAssembly.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryWordsAssembly.swift
+//  Find-and-Learn
+//
+//  Created by Руслан on 05.04.2022.
+//
+
+import Foundation
+import UIKit
+
+final class HistoryWordsAssembly: AssemblyProtocol {
+    static func assemble() -> UIViewController {
+        let router = HistoryWordsRouter()
+        let interactor = HistoryWordsInteractor(dataManager: DataManagerMock())
+        let presenter = HistoryWordsPresenter(interactor: interactor, router: router)
+        let view = HistoryWordsViewController(output: presenter)
+        
+        presenter.view = view
+        router.view = view
+        
+        return view
+    }
+}
