806b0a37641830c91a2130c4004d099f7a2378c9
Snitsaryuk Roman <nits14@mail.ru>
2022-04-08 18:01:02 +0300

    added interactor

---
+++ Source/Scenarios/Main/Account/Interactor/AccountInteractor.swift
@@ -0,0 +1,11 @@
+//
+//  AccountInteractor.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 08.04.2022.
+//
+
+import Foundation
+
+final class AccountInteractor: AccountInteractorProtocol {
+}
---
+++ Source/Scenarios/Main/Account/Interactor/AccountInteractorProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  AccountInteractorProtocol.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 08.04.2022.
+//
+
+import Foundation
+
+protocol AccountInteractorProtocol: AnyObject {
+}
