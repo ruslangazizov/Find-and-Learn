c1a9f43e8ae3f660a0c532adc81a83565f29534d
Snitsaryuk Roman <nits14@mail.ru>
2022-04-18 17:44:33 +0300

    added empty presenter

---
+++ Source/Scenarios/Main/Account/Modules/Change Password/Presenter/ChangePasswordPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  ChangePasswordPresenter.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 18.04.2022.
+//
+
+import Foundation
+
+final class ChangePasswordPresenter: ChangePasswordViewOutput {
+    // MARK: Dependencies
+    
+    weak var view: ChangePasswordViewInput?
+}
