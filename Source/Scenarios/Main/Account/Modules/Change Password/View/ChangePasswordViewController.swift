57d39ed2a7fbf8f140206bbed476fd29ea86eb9d
Snitsaryuk Roman <nits14@mail.ru>
2022-04-18 17:46:46 +0300

    added injection for vc

--- Source/Scenarios/Main/Account/Modules/Change Password/View/ChangePasswordViewController.swift
+++ Source/Scenarios/Main/Account/Modules/Change Password/View/ChangePasswordViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 final class ChangePasswordViewController: UIViewController {
+    // MARK: UI
+    
+    // MARK: Dependencies
+    
+    private let presenter: ChangePasswordViewOutput
+    
+    // MARK: Init
+    
+    init(presenter: ChangePasswordViewOutput) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
