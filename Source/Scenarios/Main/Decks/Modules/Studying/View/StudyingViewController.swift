a2e026702be2d42ed2d5e1baef1f690cca404351
Snitsaryuk Roman <nits14@mail.ru>
2022-04-23 12:24:02 +0300

    added view input + vc

---
+++ Source/Scenarios/Main/Decks/Modules/Studying/View/StudyingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StudyingViewController.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 23.04.2022.
+//
+
+import UIKit
+
+final class StudyingViewController: UIViewController {
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Source/Scenarios/Main/Decks/Modules/Studying/View/StudyingViewInput.swift
@@ -0,0 +1,11 @@
+//
+//  StudyingViewInput.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 23.04.2022.
+//
+
+import Foundation
+
+protocol StudyingViewInput: AnyObject {
+}
