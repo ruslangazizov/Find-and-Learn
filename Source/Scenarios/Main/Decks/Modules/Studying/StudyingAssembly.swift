b3fb5acc9b74279226c9e919e498c94f41ae0fec
Snitsaryuk Roman <nits14@mail.ru>
2022-04-25 16:40:12 +0300

    deleted data

--- Source/Scenarios/Main/Decks/Modules/Studying/StudyingAssembly.swift
+++ Source/Scenarios/Main/Decks/Modules/Studying/StudyingAssembly.swift
@@ -15,13 +15,6 @@ final class StudyingAssembly: AssemblyProtocol {
         
         let presenter = StudyingPresenter(router: router, interactor: interactor)
         let viewController = StudyingViewController(presenter: presenter)
-        // TODO: delete when will be merge
-        viewController.cards = [
-            FlashCard(frontSide: "1", backSide: "1", comment: "1"),
-            FlashCard(frontSide: "2", backSide: "2", comment: "2"),
-            FlashCard(frontSide: "3", backSide: "3", comment: "3"),
-            FlashCard(frontSide: "4", backSide: "4", comment: "4")
-        ]
         
         presenter.view = viewController
         router.view = viewController
