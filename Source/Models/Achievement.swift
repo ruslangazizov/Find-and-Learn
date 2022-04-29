c71af34917ebbeda66fc29dd02e471f36906cb6f
Snitsaryuk Roman <nits14@mail.ru>
2022-04-29 21:26:11 +0300

    created Achievement model

---
+++ Source/Models/Achievement.swift
@@ -0,0 +1,15 @@
+//
+//  Achievement.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 29.04.2022.
+//
+
+import Foundation
+import UIKit
+
+struct Achievement {
+    let name: String
+    let image: UIImage?
+    let dateOfGetting: String?
+}
