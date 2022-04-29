38ced85835b0ec37d909cc407f565e35cb2cdeea
Snitsaryuk Roman <nits14@mail.ru>
2022-04-29 21:06:04 +0300

    created view with protocol

---
+++ Source/Scenarios/Main/Account/Modules/Achievements/View/AchievementsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AchievementsViewController.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 29.04.2022.
+//
+
+import UIKit
+
+final class AchievementsViewController: UIViewController {
+
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Source/Scenarios/Main/Account/Modules/Achievements/View/AchievementsViewInput.swift
@@ -0,0 +1,11 @@
+//
+//  AchievementsViewInput.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 29.04.2022.
+//
+
+import Foundation
+
+protocol AchievementsViewInput: AnyObject {
+}
