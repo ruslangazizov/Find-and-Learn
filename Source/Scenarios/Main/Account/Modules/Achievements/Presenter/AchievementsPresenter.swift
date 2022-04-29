cdd0d311c8773f8328d41aacf73dc0ade6bc7e8f
Snitsaryuk Roman <nits14@mail.ru>
2022-04-29 21:07:16 +0300

    created presenter

---
+++ Source/Scenarios/Main/Account/Modules/Achievements/Presenter/AchievementsPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  AchievementsPresenter.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 29.04.2022.
+//
+
+import Foundation
+
+protocol AchievementsViewOutput: AnyObject {
+}
+
+final class AchievementsPresenter: AchievementsViewOutput {
+    // MARK: AchievementsViewOutput
+}
