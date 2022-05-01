414aa3b6ef6e3d3d574ce067b4b4612158396866
Snitsaryuk Roman <nits14@mail.ru>
2022-05-01 13:10:10 +0300

    added tab bar

---
+++ Source/Scenarios/Main/TabBar/TabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TabBarViewController.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 30.04.2022.
+//
+import Foundation
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureAppearance()
+        setupControllers()
+    }
+    
+    // MARK: Private
+    
+    private func configureAppearance() {
+        tabBar.tintColor = R.color.tabBarActiveIconColor()
+        tabBar.barTintColor = .white
+        
+        let borderView = UIView(frame: .init(
+            x: 0,
+            y: 0,
+            width: tabBar.frame.width,
+            height: .defaultBorderWidth)
+        )
+        borderView.backgroundColor = R.color.tabBarBorderColor()
+        tabBar.addSubview(borderView)
+    }
+    
+    private func setupControllers() {
+        let viewControllers = TabBarItem.allCases.map { (item: TabBarItem) -> UIViewController in
+            let controller = item.asController
+            controller.tabBarItem = item.asTabBarItem
+            return controller
+        }
+        setViewControllers(viewControllers, animated: true)
+    }
+}
