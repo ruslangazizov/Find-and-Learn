d1293b5ee363770e772bc4c9ae3d0c5d18574ff0
Snitsaryuk Roman <nits14@mail.ru>
2022-05-02 14:11:33 +0300

    changed colors for favorite words vc

--- Source/Scenarios/Main/Dictionary/Modules/Favorite Words/View/FavoriteWordsTableViewCell.swift
+++ Source/Scenarios/Main/Dictionary/Modules/Favorite Words/View/FavoriteWordsTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 final class FavoriteWordsTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+        backgroundColor = R.color.defaultBackgroundColor()
     }
     
     required init?(coder: NSCoder) {
