67270ea09b5a1304434075d01978f76077161935
Snitsaryuk Roman <nits14@mail.ru>
2022-05-14 12:19:33 +0300

    added creating deck request

---
+++ Source/Services/Network Manager/Requests/CreateDeckRequest.swift
@@ -0,0 +1,32 @@
+//
+//  CreateDeckRequest.swift
+//  Find-and-Learn
+//
+//  Created by Роман Сницарюк on 14.05.2022.
+//
+
+import Foundation
+
+struct CreateDeckRequestModel: Encodable {
+    let id: Int
+    let name: String
+    let userID: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case userID = "user_id"
+    }
+}
+
+struct CreateDeckRequest: Request {
+    let method: HTTP.Method = .post
+    let path: String = "decks/"
+    let body: HTTP.Body
+    let token: String
+    
+    init(_ model: CreateDeckRequestModel, _ token: String) {
+        self.token = token
+        body = .model(.init(model))
+    }
+}
