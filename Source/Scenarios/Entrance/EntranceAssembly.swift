58a4a0b7faeb03a62df479545e927790c70f5131
Ruslan Gazizov <ruslangazizov36@gmail.com>
2022-06-03 16:19:54 +0300

    Fix registration button for guest user type

--- Source/Scenarios/Entrance/EntranceAssembly.swift
+++ Source/Scenarios/Entrance/EntranceAssembly.swift
@@ -10,11 +10,28 @@ import UIKit
 import Swinject
 
 enum EntranceAssembly {
-    static func assemble(using container: Container) -> UIViewController {
-        let startViewController = UINavigationController(
-            rootViewController: AuthorizationAssembly.assemble(using: container)
-        )
+    static func assemble(
+        using container: Container,
+        rootModule: EntranceModuleType = .authorization
+    ) -> UIViewController {
+        let rootViewController: UIViewController
+        switch rootModule {
+        case .authorization:
+            rootViewController = AuthorizationAssembly.assemble(using: container)
+        case .registration:
+            rootViewController = RegistrationAssembly.assemble(using: container)
+        case .passwordRecovery:
+            rootViewController = PasswordRecoveryAssembly.assemble(using: container)
+        }
+        
+        let startViewController = UINavigationController(rootViewController: rootViewController)
         startViewController.isNavigationBarHidden = true
         return startViewController
     }
+}
+
+enum EntranceModuleType {
+    case authorization
+    case registration
+    case passwordRecovery
 }
